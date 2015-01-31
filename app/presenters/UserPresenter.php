ef05609a61a596910d13ecb5a3b22e9950f5693e
fishi <fishi@67cf69ce-a6f9-11e4-9d59-6930ca6ce882>
2015-01-31 09:47:30 +0000

    Forgot Password & Send Activate Code

--- app/presenters/UserPresenter.php
+++ app/presenters/UserPresenter.php
@@ -33,6 +33,13 @@ class UserPresenter extends BasePresenter
 		$this['changeEmail']->setDefaults($user->toArray());
 	}
 	
+	public function actionPassword()
+	{
+		$user = $this->getHttpRequest()->getQuery('code');
+		$pole = array('code' => $user);
+		$this['changePassword1']->setDefaults($pole);
+	}
+	
 	protected function createComponentRegisterForm()
 	{
 		$form = new Nette\Application\UI\Form;
@@ -140,6 +147,35 @@ class UserPresenter extends BasePresenter
 		$this->database->table('users')->where('username',$user)->update($values);
 	}
 	
+	protected function createComponentChangePassword1()
+	{
+		$form = new Nette\Application\UI\Form;
+		
+		$form->addHidden('code');
+		
+		$form->addPassword('password', 'Heslo:')
+			->setRequired('Prosím vyplňte heslo.');
+		
+		$form->addPassword('passwordVerify', 'Heslo znovu:')
+			->setRequired('Zadejte prosím heslo ještě jednou pro kontrolu')
+			->addRule(Form::EQUAL, 'Hesla se neshodují', $form['password']);
+		
+		$form->addSubmit('edit', 'Upravit');
+
+		$form->onSuccess[] = $this->changePassword1FormSucceeded;
+		return $form;
+	}
+	
+	public function changePassword1FormSucceeded($form)
+	{
+		$values = $form->getValues();
+		$pswd = $values["password"];
+		$code = $values["code"];
+		$this->database->query('UPDATE users SET password = SHA1("'.$pswd.'") WHERE SHA1(username) = "'.$code.'"');
+		$this->flashMessage('Heslo bylo změneno.', 'success');
+		$this->redirect('Sign:in');
+	}
+	
 	protected function createComponentDefaultCurrency()
 	{
 		$form = new Nette\Application\UI\Form;
@@ -160,5 +196,66 @@ class UserPresenter extends BasePresenter
 		$values = $form->getValues();
 		$user = $this->user->getIdentity()->getId();
 		$this->database->table('users')->where('username',$user)->update($values);
+	}
+	
+	protected function createComponentSendCode()
+	{
+		$form = new Nette\Application\UI\Form;
+		$form->addText('email', 'Email:')
+			->addRule(FORM::EMAIL, 'Zadejte prosím e-mail ve správném tvaru.')
+			->setRequired('Prosím vyplňte email.');
+		
+		$form->addSubmit('edit', 'Zaslat kód');
+
+		$form->onSuccess[] = $this->sendCodeFormSucceeded;
+		return $form;
+	}
+	
+	public function sendCodeFormSucceeded($form)
+	{
+		$values = $form->getValues();
+		$user = $this->database->table('users')->where('email',$values['email'])->fetch();
+		$mail = new Message;
+		$mail->setFrom('accounts@mywal.com')
+			->addTo($values['email'])
+			->setSubject('Váš aktivační kód')
+			->setBody("Dobrý den, \n Na Váš požadavek Vám byl zaslán aktivační kód. \n"
+					. "Pro aktivaci účtu klikněte zde: "
+					//."http://mywallet.eu/user/activate?=".$code
+					. "http://localhost/MyWallet/www/user/activate?code=".sha1($user->username)
+					. ". Přejeme mnoho bezstarostných výdajů.\n\n Váš MyWallet tým.");
+		
+		$mailer = new SendmailMailer;
+		$mailer->send($mail);
+	}
+	
+	protected function createComponentSendPassword()
+	{
+		$form = new Nette\Application\UI\Form;
+		$form->addText('email', 'Email:')
+			->addRule(FORM::EMAIL, 'Zadejte prosím e-mail ve správném tvaru.')
+			->setRequired('Prosím vyplňte email.');
+		
+		$form->addSubmit('edit', 'Zaslat kód');
+
+		$form->onSuccess[] = $this->sendPasswordFormSucceeded;
+		return $form;
+	}
+	
+	public function sendPasswordFormSucceeded($form)
+	{
+		$values = $form->getValues();
+		$user = $this->database->table('users')->where('email',$values['email'])->fetch();
+		$mail = new Message;
+		$mail->setFrom('accounts@mywal.com')
+			->addTo($values['email'])
+			->setSubject('Zapomenuté heslo')
+			->setBody("Dobrý den, \n Heslo si můžete změnit zde: \n"
+					//."http://mywallet.eu/user/activate?=".$code
+					. "http://localhost/MyWallet/www/user/password?code=".sha1($user->username)
+					. ". Přejeme mnoho bezstarostných výdajů.\n\n Váš MyWallet tým.");
+		
+		$mailer = new SendmailMailer;
+		$mailer->send($mail);
 	}
 }
